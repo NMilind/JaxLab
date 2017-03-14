bb50881687884a989f0ab97456f37f7a3f13f2b7
nikhil <nikhil@nikhil-laptop>
2017-03-14 15:40:27 -0400

    UPDATE

--- src/Pairwise.R
+++ src/Pairwise.R
@@ -193,3 +193,8 @@ pairs.double(data1=liver.rz, genes1=c("Fbn1", "Gprc5b", "Rabgap1,Gpr21", "Trib3"
 pairs.phenos.simple(data.g=adipose.rz, genes=c("Amy1", "Agl", "Fabp2", "Cyp2u1", "Fbn1"), data.p=phenotypes.rz, phenos=c("HOMA.10wk"))
 pairs.phenos.simple(data.g=adipose.rz, genes=c("Lipe", "Cyp2s1", "Cyp2b10", "Foxa3", "Apoc4", "Apoc2", "Apoe", "Gsk3a", "Fbn1"), data.p=phenotypes.rz, phenos=c("HOMA.10wk"))
 pairs.phenos.simple(data.g=adipose.rz, genes=c("Amy1", "Cyp2s1", "Apoc4", "Apoc2", "Apoe", "Fbn1"), data.p=phenotypes.rz, phenos=c("HOMA.10wk"))
+
+## Prelim Studies
+pairs.phenos.simple(genes=c("Fbn1", "Bgn", "Col1a1", "Col1a2", "Col6a1"), data.g=adipose.rz, phenos=c("HOMA.8wk", "INS.10wk"), data.p=phenotypes.rz)
+pairs.phenos.simple(genes=c("Fbn1", "Col6a2", "Col16a1", "Efemp2", "Fkbp10"), data.g=adipose.rz, phenos=c("HOMA.8wk", "INS.10wk"), data.p=phenotypes.rz)
+pairs.phenos.simple(genes=c("Fbn1", "Mrgprf", "Nfatc4", "Timp1", "Timp2"), data.g=adipose.rz, phenos=c("HOMA.8wk", "INS.10wk"), data.p=phenotypes.rz)
--- src/Scrubber.R
+++ src/Scrubber.R
@@ -6,9 +6,49 @@
 source("src/configuration.R")
 source("src/ScanOne.R")
 
+#################################################
+## FULL SCRUB                                  ##
+#################################################
+
+THRESHOLD <- 0.35
+INS.THRESHOLD <- 0.20
+HOMA.THRESHOLD <- 0.20
+FAT.THRESHOLD <- 0.20
+
+genes <- c()
+for (i in 1:nrow(annot)) {
+  
+  gene <- annot[i,"gene_symbol"]
+  if (gene.data.exists(gene)) {
+    data1 <- gene.exp("Fbn1", adipose.rz)
+    data.ins <- clinical("INS.10wk", phenotypes.rz)
+    data.homa <- clinical("HOMA.8wk", phenotypes.rz)
+    data.fat <- clinical("Fat.wt", phenotypes.rz)
+    data2 <- gene.exp(gene, adipose.rz)
+    indx <- sort(unique(c(which(is.na(data1)), which(is.na(data.ins)), which(is.na(data.homa)), which(is.na(data.fat)), which(is.na(data2)))))
+    data1 <- data1[-indx]
+    data.ins <- data.ins[-indx]
+    data.homa <- data.homa[-indx]
+    data.fat <- data.fat[-indx]
+    data2 <- data2[-indx]
+    correlation <- abs(cor(x=data1, y=data2))
+    correlation.ins <- abs(cor(x=data.ins, y=data2))
+    correlation.homa <- abs(cor(x=data.homa, y=data2))
+    correlation.fat <- abs(cor(x=data.fat, y=data2))
+    if ((correlation >= THRESHOLD && correlation < 0.40) && (correlation.ins >= INS.THRESHOLD && correlation.homa >= HOMA.THRESHOLD && correlation.fat >= FAT.THRESHOLD)) {
+      print(paste("Gene: ", gene, " | Correlation: ", correlation, " | Insulin: ", correlation.ins, " | HOMA: ", correlation.homa, " | Fat.wt: ", correlation.fat, sep=""))
+      genes <- c(genes, paste(gene))
+    }
+  }
+}
+
+#################################################
+## PARTIAL SCRUB                               ##
+#################################################
+
 CSV <- "data/chr-7-genes.csv"
-PDF <- "report/paper/genes/chr3.pdf"
-THRESHOLD <- 0.15
+PDF <- "report/paper/genes/Ins-0.35-ins-0.20-homa-0.20-fat-0.20.pdf"
+THRESHOLD <- 0.20
 
 chrs <- read.csv(CSV)
 genes <- c()
@@ -29,35 +69,40 @@ for (i in 1:nrow(chrs)) {
   }  
 }
 
-pdf(PDF)
-graphics.allow.open <- FALSE
-scan.fbn1 <- runScanOne(gene.exp("Fbn1", adipose.rz), "Fbn1.adipose.perms.RData", perms=100, clean=TRUE, graphs=FALSE)
-for (i in 1:length(genes)) {
-  
-  name <- genes[i]
-  covar <- gene.exp(genes[i], adipose.rz)
-  
-  scan.fbn1.add.apoe <- runScanOne.AddCovar(gene.exp("Fbn1", adipose.rz), "Fbn1.adipose.perms.RData", covar, 100, clean=TRUE, graphs=FALSE)
-  scan.fbn1.int.apoe <- runScanOne.IntCovar(gene.exp("Fbn1", adipose.rz), "Fbn1.adipose.perms.RData", covar, 100, clean=TRUE, graphs=FALSE)
-  
-  par(mfrow=c(3,1))
-  
-  plot(scan.fbn1$scan, lodcolumn=1, main="FBN1 LOD for Variation in Mouse Genome")
-  add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
-  add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
-  add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
-  
-  plot(scan.fbn1.add.apoe$scan, lodcolumn=1, main=paste("FBN1 with ", name, " as Additive Covariate", sep=""))
-  add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
-  add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
-  add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
-  
-  plot(scan.fbn1.int.apoe$scan, lodcolumn=1, main=paste("FBN1 with ", name, " as Interactive Covariate", sep=""))
-  add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
-  add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
-  add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
-  
-  par(mfrow=c(1,1))
+run <- function() {
+  pdf(PDF)
+  graphics.allow.open <- FALSE
+  scan.fbn1 <- runScanOne(clinical("INS.10wk", phenotypes.rz), "INS.10wk.perms.RData", perms=100, clean=TRUE, graphs=FALSE)
+  for (i in 1:length(genes)) {
+    
+    print(genes[i])
+    name <- genes[i]
+    covar <- gene.exp(genes[i], adipose.rz)
+    
+    scan.fbn1.add.apoe <- runScanOne.AddCovar(clinical("INS.10wk", phenotypes.rz), "INS.10wk.perms.RData", covar, 100, clean=TRUE, graphs=FALSE)
+    scan.fbn1.int.apoe <- runScanOne.IntCovar(clinical("INS.10wk", phenotypes.rz), "INS.10wk.perms.RData", covar, 100, clean=TRUE, graphs=FALSE)
+    
+    par(mfrow=c(3,1))
+    
+    plot(scan.fbn1$scan, lodcolumn=1, main="INS.10wk LOD for Variation in Mouse Genome")
+    add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
+    add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
+    add.threshold(scan.fbn1$scan, perms=scan.fbn1$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
+    
+    plot(scan.fbn1.add.apoe$scan, lodcolumn=1, main=paste("INS.10wk with ", name, " as Additive Covariate", sep=""))
+    add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
+    add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
+    add.threshold(scan.fbn1.add.apoe$scan, perms=scan.fbn1.add.apoe$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
+    
+    plot(scan.fbn1.int.apoe$scan, lodcolumn=1, main=paste("INS.10wk with ", name, " as Interactive Covariate", sep=""))
+    add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.05, lty="dashed", lwd=1, col="green")
+    add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.10, lty="dashed", lwd=1, col="orange")
+    add.threshold(scan.fbn1.int.apoe$scan, perms=scan.fbn1.int.apoe$perms, alpha=0.63, lty="dashed", lwd=1, col="red")
+    
+    par(mfrow=c(1,1))
+  }
+  dev.off()
+  graphics.allow.open <- TRUE
 }
-dev.off()
-graphics.allow.open <- TRUE
+
+run()
